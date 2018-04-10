637cd2da92224d7ceb43f8585b67d4a9290c8733
enderloong <yloongli@gmail.com>
2018-04-10 17:03:43 +0800

    assign2-q2 ver 0.1

--- Assignment2/boundary_extraction.m
+++ Assignment2/boundary_extraction.m
@@ -15,12 +15,24 @@ function boundary_extraction()
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % TODO_1: Apply erosion on the image
     % img_erode = ??
+    erosion = [-1 -1; -1 0; -1 1;0 -1; 0 0; 0 1; 1 -1; 1 0; 1 1];
+    e_size = size(erosion, 1);
+    [m, n] = size(img);
+    img_proc = zeros([m,n,e_size]);
+    for i = 1:e_size
+        img_proc(max(1, 1 + erosion(i,1)):min(m, m + erosion(i,1)), max(1, 1 + erosion(i,2)):min(n, n + erosion(i,2)),i) = ...
+        img(max(1, 1 - erosion(i,1)):min(m, m - erosion(i,1)), max(1, 1 - erosion(i,2)):min(n, n - erosion(i,2)));
+    end
+    img_erode = zeros([m,n]);
+    img_erode(sum(img_proc, 3) == 9) = 1;
+    img_erode = logical(img_erode);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % TODO_1: Apply erosion on the image
     % img_boundary = ??
+    img_boundary = logical(img .* (~img_erode));
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
@@ -29,7 +41,3 @@ function boundary_extraction()
     imshow(img_boundary);title('Boundary image');
 
 end
-    
-    
-    
-    
